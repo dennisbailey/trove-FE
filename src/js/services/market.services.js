80918b60e7fc2861cc9a4659ac504a710cbe6980
Dennis Bailey <bailey.dennis@gmail.com>
2016-06-05 11:20:25 -0600

    Added an api call for vendor info

--- src/js/services/market.services.js
+++ src/js/services/market.services.js
@@ -23,6 +23,10 @@ function marketService($http, $window, $rootScope) {
     getMarketImages: function(id) {
       return $http.get('https://trove-api.herokuapp.com/v1/images?id=' + id + '');
     },
+    
+    getMarketVendors: function(fmid) {
+      return $http.get(/* https://trove-api.herokuapp.com */'http://localhost:5000/v1/vendors?fmid=' + fmid + '');
+    },
   }
 
 };
